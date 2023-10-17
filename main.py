48d459d6009970364c0220fa5576892b9c8b8564
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-10-17 15:29:40 +0300

    hw19.1 t2 completed

---
+++ main.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def get_content(self):
+        f = open('hw19_1.html', 'r', encoding="utf-8")
+        r = f.read()
+        return f"""{r}"""
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        query_comp = parse_qs(urlparse(self.path).query)
+        print(query_comp)
+        self.content = self.get_content()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(self.content, "utf-8"))
+
+if __name__ == "__main__":
+
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+
+    webServer.server_close()
+    print("Server stopped.")
